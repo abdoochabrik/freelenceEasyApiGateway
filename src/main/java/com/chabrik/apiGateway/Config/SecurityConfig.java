9b3cfb87ed1159b18c40b5b0a2021008a3b0bf07
Abdellatif Chabrik <achabrik@irevolution.com>
2023-12-31 11:38:27 +0100

    keycloak configs

---
+++ src/main/java/com/chabrik/apiGateway/Config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.chabrik.apiGateway.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        serverHttpSecurity.authorizeExchange(exchange -> exchange.pathMatchers("/api/v1/users/register")
+                                                      .permitAll()
+                                                      .anyExchange()
+                                                      .authenticated())
+                           .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+        return serverHttpSecurity.build();                           
+    }
+}
